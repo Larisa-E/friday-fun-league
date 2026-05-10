963152dfccdf83848b2ffd17fd6d4f44ff9d9c22
Larisa-E <63336@edu.sde.dk>
2026-05-10 20:32:57 +0200

    Test match stats cache invalidation

--- tests/Feature/StatsPageCacheTest.php
+++ tests/Feature/StatsPageCacheTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Participant;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Cache;
 use Tests\TestCase;
@@ -27,5 +28,37 @@ class StatsPageCacheTest extends TestCase
         $this->get(route('stats'))
             ->assertOk()
             ->assertSee('Charlie');
+    }
+
+    public function test_match_changes_refresh_the_cached_stats_page(): void
+    {
+        config()->set('cache.default', 'file');
+        Cache::flush();
+
+        $winner = Participant::create([
+            'name' => 'Ada',
+            'avatar_emoji' => null,
+        ]);
+
+        $loser = Participant::create([
+            'name' => 'Ben',
+            'avatar_emoji' => null,
+        ]);
+
+        $this->get(route('stats'))
+            ->assertOk()
+            ->assertDontSee('Chess');
+
+        $this->post(route('matches.store'), [
+            'winner_id' => $winner->id,
+            'loser_id' => $loser->id,
+            'winner_score' => 5,
+            'loser_score' => 3,
+            'game_type' => 'Chess',
+        ])->assertRedirect(route('dashboard'));
+
+        $this->get(route('stats'))
+            ->assertOk()
+            ->assertSee('Chess');
     }
 }
